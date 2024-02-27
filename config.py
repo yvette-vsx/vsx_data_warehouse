7a6a134f5bd9eddecd0191ee7a834d8d9ef2a6cc
Yvette Yu <yvette.sh.yu@viewsonic.com>
2024-02-27 17:27:10 +0800

    Modified config.py. Changed configuration lib from configparser to dotenv.

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
-import configparser
+import os
+from dotenv import load_dotenv
 
-config = configparser.ConfigParser()
-config.read("config.ini")
+load_dotenv()
 
 PG_WH_PROD_CONFIG = {
-    "host": config["dw.pg.prod"]["host"],
-    "user": config["dw.pg.prod"]["user"],
-    "password": config["dw.pg.prod"]["pwd"],
-    "port": config["dw.pg.prod"]["port"],
+    "host": os.getenv("DW_PG_PROD_HOST"),
+    "user": os.getenv("DW_PG_PROD_USER"),
+    "password": os.getenv("DW_PG_PROD_PWD"),
+    "port": os.getenv("DW_PG_PROD_PORT"),
 }
 
 PG_WH_DEV_CONFIG = {
-    "host": config["dw.pg.dev"]["host"],
-    "user": config["dw.pg.dev"]["user"],
-    "password": config["dw.pg.dev"]["pwd"],
-    "port": config["dw.pg.dev"]["port"],
+    "host": os.getenv("DW_PG_DEV_HOST"),
+    "user": os.getenv("DW_PG_DEV_USER"),
+    "password": os.getenv("DW_PG_DEV_PWD"),
+    "port": os.getenv("DW_PG_DEV_PORT"),
 }
 _PG_DW_CONNECT_URL = (
     "postgresql+psycopg2://%(user)s:%(password)s@%(host)s:%(port)s/warehouse"
@@ -23,13 +23,13 @@ PG_PROD_CONNECT_URL = _PG_DW_CONNECT_URL % PG_WH_PROD_CONFIG
 PG_DEV_CONNECT_URL = _PG_DW_CONNECT_URL % PG_WH_DEV_CONFIG
 
 MIX_PANEL_SVC_ACCOUNT_PROD = (
-    config["mixpanel.service.account.prod"]["user"],
-    config["mixpanel.service.account.prod"]["secret"],
+    os.getenv("MX_SVC_AC_PROD_USER"),
+    os.getenv("MX_SVC_AC_PROD_SECRET"),
 )
-MIX_PANEL_CS_PROJECT_ID_PROD = config["mixpanel.service.account.prod"]["peojectid"]
+MIX_PANEL_CS_PROJECT_ID_PROD = os.getenv("MX_SVC_AC_PROD_PROJECT_ID")
 
 MIX_PANEL_SVC_ACCOUNT_STG = (
-    config["mixpanel.service.account.stg"]["user"],
-    config["mixpanel.service.account.stg"]["secret"],
+    os.getenv("MX_SVC_AC_STAG_USER"),
+    os.getenv("MX_SVC_AC_STAG_SECRET"),
 )
-MIX_PANEL_CS_PROJECT_ID_STG = config["mixpanel.service.account.stg"]["peojectid"]
+MIX_PANEL_CS_PROJECT_ID_STG = os.getenv("MX_SVC_AC_STAG_PROJECT_ID")
